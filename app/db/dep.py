ac0625c47d2e6b9fbb84aabea59f2cf66a97e1c7
Neykuratick <neykuratick@yahoo.com>
2023-02-18 14:04:34 +0300

    фиксы вадима

--- app/db/dep.py
+++ app/db/dep.py
@@ -1,10 +1,11 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 engine = create_async_engine(settings.POSTGRES_URL_ASYNC, echo=False)
-async_session = sessionmaker(  # noqa
+async_session = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
